4f24b68dc27ab9bd907a9cfaef3ef25452cc4085
James McMahon <james.mcmahon@phs.scot>
2021-08-09 14:53:18 +0100

    Add by option to allow plotting variation

--- R/plot_usage.R
+++ R/plot_usage.R
@@ -5,7 +5,7 @@
 #'
 #' @return a plot object from [ggplot][tibble::tibble-package]
 #' @export
-plot_usage <- function(usage_data, drive_name) {
+plot_usage <- function(usage_data, drive_name, by = "user") {
   if (!requireNamespace("ggplot2", quietly = TRUE)) {
     stop("Package \"ggplot2\" needed to plot data usage. Please install it.",
       call. = FALSE
@@ -14,20 +14,31 @@ plot_usage <- function(usage_data, drive_name) {
 
   plot <- ggplot2::ggplot(
     usage_data,
-    ggplot2::aes(x = .data$full_name, y = .data$size, fill = .data$file_type)
+    ggplot2::aes(y = .data$size, fill = .data$file_type_lumped)
   ) +
-    ggplot2::geom_bar(stat = "identity") +
     ggplot2::theme_minimal() +
+    ggplot2::ggtitle(drive_name) +
+    ggplot2::scale_fill_brewer("File Type", type = "qual") +
+    ggplot2::ylab("Space used") +
     ggplot2::theme(
-      axis.text.x = ggplot2::element_text(angle = 45, hjust = 1),
+      axis.text.x = ggplot2::element_text(angle = 35, hjust = 1),
       legend.position = "top"
-    ) +
-    ggplot2::labs(
-      x = "Person",
-      y = paste("Total space used -", .data$drive_name)
-    ) +
-    ggplot2::ggtitle(.data$drive_name) +
-    ggplot2::scale_fill_brewer(palette = "Set3")
+    )
+
+  if (by == "user") {
+    plot <- plot +
+      ggplot2::geom_col(ggplot2::aes(x = .data$full_name)) +
+      ggplot2::xlab("User")
+  } else if (by == "folder") {
+    plot <- plot +
+      ggplot2::geom_col(ggplot2::aes(x = .data$top_folder)) +
+      ggplot2::xlab("Top-level folder")
+  } else if (by == "all") {
+    plot <- plot +
+      ggplot2::geom_col(ggplot2::aes(x = .data$full_name)) +
+      ggplot2::xlab("User") +
+      ggplot2::facet_wrap("top_folder")
+  }
 
   return(plot)
 }
